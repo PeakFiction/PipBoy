c11a66fec6e3121be7413629d0db73c66dd0ca43
peakfiction <sakhran.t@gmail.com>
2023-09-20 08:05:16 +0700

    Assignment 3 Push

--- main/models.py
+++ main/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
-    description = models.TextField()
+    value = models.IntegerField()
     description = models.TextField()
     version = models.TextField()
     releasedate = models.TextField()
